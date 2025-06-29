ae39476bea6405469a0d64911d280c736da9112d
Linus Bensryd <linne01lampa@gmail.com>
2025-06-29 14:49:39 +0200

    Show title and link of product

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://www.tehusetjava.se/category/veckans-te"
+base_url = "https://www.tehusetjava.se"
+veckans_url = base_url + "/category/veckans-te"
 
 headers = {"UserAgent": "Mozzila/5.0"}
-response = requests.get(url, headers=headers)
+response = requests.get(veckans_url, headers=headers)
 
 soup = BeautifulSoup(response.text, "html.parser")
 
-matching_spans = soup.find_all("span", string="Veckans Te")
+list_items = soup.find_all("li", class_="gallery-item")
 
-for span in matching_spans:
-    print("Found:", span)
+for li_item in list_items:
+    anchor_tag = li_item.find("a", class_="gallery-info-link")
+
+    if anchor_tag:
+        veckans_te_span = anchor_tag.find("span", string="Veckans te")
+
+        if veckans_te_span:
+            product_title_h3 = anchor_tag.find("h3")
+            if product_title_h3:
+                title_text = product_title_h3.get_text(strip=True)
+                link = base_url + str(anchor_tag.get("href"))
+                print(
+                    "Found Product Title: {title}, Link: {link}".format(
+                        title=title_text, link=link
+                    )
+                )
+            else:
+                print("No <h3> tag found within this product link.")
+        else:
+            pass
+    else:
+        print("No appropriate <a> tag found in this <li> item.")
